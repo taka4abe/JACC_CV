799086c10f8643845d6f361a95686eaf8a8587d8
Takashi <taka4abe@gmail.com>
2018-12-13 16:40:34 +0900

    Update test_DCNN.py

--- test_DCNN.py
+++ test_DCNN.py
@@ -3,18 +3,19 @@
 # This is the part of the codes used for the article entitled "A Deep Learning
 # Approach for Assessment of Regional Wall Motion Abnormality from
 # Echocardiographic Images" for JACC CV imaging.
+#
+# Before using this code, please prepare image data at "./data_folder" dir.
+#
+# ./data_folder/train/Norm
+# ./data_folder/train/LAD
+# ./data_folder/train/LCXD
+# ./data_folder/train/RCA
+#
+# ./data_folder/test/Norm
+# ./data_folder/test/LAD
+# ./data_folder/test/LCX
+# ./data_folder/test/RCA
 
-# Before using this code, we should prepare image data at "/data_folder" dir.
-#
-# /data_folder/train/Norm
-# /data_folder/train/LAD
-# /data_folder/train/LCXD
-# /data_folder/train/RCA
-#
-# /data_folder/test/Norm
-# /data_folder/test/LAD
-# /data_folder/test/LCX
-# /data_folder/test/RCA
 #
 # Each dir should have echocardiographic images (.png is recommended and .jpg
 # acceptable) that contains endo-diastolic, mid-systolic, and endo-systolic
